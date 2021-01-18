4deb902982185106fe245e70bd5af9c3043d130d
resie001 <aderesie@zohomail.com>
2021-01-18 23:32:18 +0700

    Menambahkan logic

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,6 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var imgDiceFirst: UIImageView!
+    @IBOutlet weak var imgDiceSecond: UIImageView!
+    @IBAction func btnRollClicked(_ sender: UIButton) {
+        imgDiceFirst.image = listImage.randomElement()
+        imgDiceSecond.image = listImage.randomElement()
+    }
+    
+    private let listImage:[UIImage] = [#imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix")]
 
     override func viewDidLoad() {
         super.viewDidLoad()
